00940840544ab9b20a7cc8fb92ae9f36ff565168
Zacarias F. Ojeda <zojeda@hexacta.com>
2015-09-28 16:58:47 -0300

    first commit

---
+++ gulp/webpack.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var gulpWebpack = require('gulp-webpack');
+
+var path = require("path");
+var webpack = require("webpack");
+
+var webpackConfig = {
+  module: {
+    entry: './src/app/app.js',
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }],
+    resolve: {
+        root: [path.join(__dirname, "bower_components")]
+    },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+        )
+    ]
+  }
+}
+
+gulp.task('webpack', function() {
+  return gulp.src('src/app/app.js')
+    .pipe(gulpWebpack(webpackConfig))
+    .pipe(gulp.dest('dist/'));
+});
